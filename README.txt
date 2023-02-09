757ee5882c4ef23bf9c9c4fab317c3052d456e73
Mishra <avinash.mishra@gartner.com>
2023-02-09 14:09:30 +0530

    test

--- README.txt
+++ README.txt
@@ -1,10 +1,12 @@
-http://localhost:8080/app/Common/home
 
 
-http://localhost:8080/app/h2-database/login.jsp
+http://localhost:8082/app/Common/home2
 
 
-http://localhost:8080/app/swagger-ui.html#/
+http://localhost:8082/app/h2-database/login.jsp
+
+
+http://localhost:8082/app/swagger-ui.html#/
 
 
 jdbc:h2:mem:avi
--- src/main/java/avi/CommonController.java
+++ src/main/java/avi/CommonController.java
@@ -4,12 +4,16 @@ package avi;
 
 import avi.dao.CommonRepository;
 import avi.service.CommonService;
+import org.hibernate.service.spi.InjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
+import java.beans.JavaBean;
 import java.net.http.HttpRequest;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/Common")
 public  class   CommonController {
 
@@ -19,12 +23,26 @@ private CommonRepository repository;
 @Autowired
 private CommonService commonService;
 
+
+
+
+
+
     @GetMapping("/home")
     public String home(HttpRequest httpRequest)
     {
 
         return "home";
     }
+
+    @GetMapping("/home2")
+    public String home()
+    {
+
+        return "home";
+    }
+
+
 
 
 
