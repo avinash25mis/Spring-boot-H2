1c8f4c0c949df630bd33ed9b40eb4c65a9e2c22a
Mishra <avinash.mishra@gartner.com>
2023-03-10 14:50:23 +0530

    tt

--- src/main/java/avi/configuration/PropertyFilesConfiguration.java
+++ src/main/java/avi/configuration/PropertyFilesConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
  * @author avinash.a.mishra
  */
 @Component
-@PropertySource({"classpath:messages/messages.properties","classpath:messages/notifications.properties"})
+//@PropertySource({"classpath:messages/messages.properties","classpath:messages/notifications.properties"})
 public class PropertyFilesConfiguration {
 
 
